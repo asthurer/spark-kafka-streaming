8114fdf7afb31a6e1ad97869745e797f7326b107
Ashish Agrawal <asthurer@gmail.com>
2024-11-26 22:34:33 +0530

    Update spark.sh

--- kafka-spark-streaming/spark.sh
+++ kafka-spark-streaming/spark.sh
@@ -1,9 +1,4 @@
-spark-submit \
---class KafkaToHDFS \
---master yarn \
---deploy-mode cluster \
---files /home/user/configs/application.conf \
-target/kafka-to-hdfs-1.0-SNAPSHOT.jar
+spark-submit --class KafkaToHDFS --master yarn --deploy-mode cluster --files /home/user/configs/application.conf target/kafka-to-hdfs-1.0-SNAPSHOT.jar
 
 
 #path to application.conf should be your user directory path
